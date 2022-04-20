e546654d37d099c4619bc66f3162fd357c232983
christophe bellec <christophe.bellec@ekim.fr>
2022-04-20 10:47:46 +0200

    index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,16 @@
+import { AuthData, AuthFacebook } from "./types/auth";
+import { Feeling } from "./types/feelings";
+import { Recommandations } from "./types/recommandations";
+
+export interface TinderApi {
+  auth: {
+    withFacebook: (authData: AuthData) => Promise<AuthFacebook>;
+  };
+  feeling: {
+    like: (userId: string) => Promise<Feeling>;
+    pass: (userId: string) => Promise<Feeling>;
+  };
+  recommandation: {
+    getRecommandations: Promise<Recommandations>;
+  };
+}
