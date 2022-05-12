12e2c2b44530fcf7e84e0edfb2fe1f579598b4a5
christophe bellec <christophe.bellec@ekim.fr>
2022-05-12 10:12:37 +0200

    fix error debug

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import auth from "./api/auth";
 import feeling from "./api/feeling";
 import recommandation from "./api/recommandation";
-import { createDb } from "./db/db";
 
 const tinderApi = {
   auth,
   feeling,
   recommandation,
 };
-
-createDb()
-
 export default tinderApi;
