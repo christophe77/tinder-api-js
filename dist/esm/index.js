329318bc4f873a5a5f67bfe0d9c0d71511d3b4fb
christophe bellec <christophe.bellec@ekim.fr>
2022-05-12 10:13:45 +0200

    1.0.12

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,11 +1,9 @@
 import auth from "./api/auth";
 import feeling from "./api/feeling";
 import recommandation from "./api/recommandation";
-import { createDb } from "./db/db";
 const tinderApi = {
     auth,
     feeling,
     recommandation,
 };
-createDb();
 export default tinderApi;
